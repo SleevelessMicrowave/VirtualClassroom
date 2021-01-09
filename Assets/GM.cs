cd873b19f499202f71608ada44cef47550f952c0
Eric Wang <eric3691215@gmail.com>
2021-01-09 13:27:39 -0500

    Scene Manager

---
+++ Assets/GM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadAssignment()
+    {
+        SceneManager.LoadScene("Assignment_Scene");
+        Debug.Log("clicked");
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("Scene");
+    }
+}
