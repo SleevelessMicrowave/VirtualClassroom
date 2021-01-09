a33f057eab0810432020d810c41153275f43cb27
Eric Wang <eric3691215@gmail.com>
2021-01-09 10:22:00 -0500

    Added Classroom Meet Drive

---
+++ Assets/Mirror/Examples/Tanks/Scripts/OpenLink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://meet.google.com/");
+    }
+
+    public void OpenClassroom()
+    {
+        Application.OpenURL("https://classroom.google.com");
+    }
+
+    public void OpenDrive()
+    {
+        Application.OpenURL("https://drive.google.com");
+    }
+}
